b2fcaa9a34ddbc748a1974e87fbd5ec2c9fa1249
ziclude <cindyzickziemi@gmail.com>
2022-06-16 15:11:28 -0700

    files added

---
+++ monty.h
@@ -0,0 +1,80 @@
+#ifndef MONTY_H
+#define MONTY_H
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <ctype.h>
+
+/**
+ * struct stack_s - doubly linked list representation of a stack (or queue)
+ * @n: integer
+ * @prev: points to the previous element of the stack (or queue)
+ * @next: points to the next element of the stack (or queue)
+ *
+ * Description: doubly linked list node structure
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct stack_s
+{
+int n;
+struct stack_s *prev;
+struct stack_s *next;
+} stack_t;
+
+/**
+ * struct instruction_s - opcode and its function
+ * @opcode: the opcode
+ * @f: function to handle the opcode
+ *
+ * Description: opcode and its function
+ * for stack, queues, LIFO, FIFO
+ */
+
+typedef struct instruction_s
+{
+char *opcode;
+void (*f)(stack_t **stack, unsigned int line_number);
+} instruction_t;
+
+/**
+ * @point: pointer
+ * Description: global variable
+ */
+
+typedef struct var
+{
+char **point
+} var_t;
+
+var_t global;
+
+char **for_line(char **inpt);
+void get_function(stack_t **tap, unsigned int line_number);
+void push(stack_t **stack, unsigned int line_number);
+void pall(stack_t **stack, unsigned int line_number);
+void pint(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
+char **for_line(char **inpt);
+void free_dble(char **p);
+void free_dlist(stack_t *hd);
+void release(FILE **streaming, char **montcod, char op);
+void add(stack_t **stack, unsigned int line_number);
+void nop(stack_t **stack, unsigned int line_number);
+void sub(stack_t **stack, unsigned int line_number);
+void div(stack_t **stack, unsigned int line_number);
+void mul(stack_t **stack, unsigned int line_number);
+void mod(stack_t **stack, unsigned int line_number);
+int check(char *num);
+void delnwline(char **del);
+int emptcod(char *string);
+void swap(stack_t **stack, unsigned int line_number);
+void pstr(stack_t **stack, unsigned int line_number);
+void pchar(stack_t **stack, unsigned int line_number);
+void rotr(stack_t **stack, unsigned int line_number);
+
+#endif /*MONTY_H*/
